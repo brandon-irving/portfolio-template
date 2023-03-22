237b4293ec6694557cfb4a5a6d6b7d0f0ae99fc1
birving2 <brandon@peakactivity.com>
2023-03-21 20:41:16 -0400

    added theme

--- src/buildConfig/core/theme.js
+++ src/buildConfig/core/theme.js
@@ -1,10 +1,10 @@
 module.exports = {
-  main: '#64ffda', // the main font color, image overlay and accent
-  mainHover: 'rgba(100, 255, 218, 0.1)',
-  pageBg: '#0a192f', // the bg color
-  boxBg: '#112240', // box colors
-  boxShadow: 'rgba(2, 12, 27, 0.7)', // box colors
-  divider: '#233554', // divider lines
-  scrollBar: '#495670', // scrollbar
-  subtext: ['#8892b0', ' #a8b2d1', '#ccd6f6'], // subtext 1, 2, 3
+  main: ' #eddcd9', // the main font color, image overlay and accent
+  mainHover: '#f2ebe9',
+  pageBg: '#264143', // the bg color
+  boxBg: '#eddcd9', // box colors
+  boxShadow: ' #e99f4c', // box colors
+  divider: ' #e99f4c', // divider lines
+  scrollBar: '#eddcd9', // scrollbar
+  subtext: ['#e99f4c', ' #de5499', '#de5499'], // subtext 1, 2, 3
 };
